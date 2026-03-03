ac5eaac7ffe455ce0946f33bdf23d0cb9acb12f9
Hemanth R <hemanthrt18@gmail.com>
2026-03-03 15:28:51 +0530

    [Hemanth] WIP: Setup pipelines & hosting

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,5 @@
-import { TextEncoder, TextDecoder } from "util";
-
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { TextEncoder, TextDecoder } = require("util");
 Object.assign(global, { TextEncoder, TextDecoder });
 
 import "@testing-library/jest-dom";
